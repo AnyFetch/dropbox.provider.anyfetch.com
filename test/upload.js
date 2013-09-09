6b25e7141682b7bef18bef9c71956b4f3e0fc651
Neamar <neamar@neamar.fr>
2013-09-09 15:52:45 +0200

    Running test suite

--- test/upload.js
+++ test/upload.js
@@ -17,15 +17,16 @@ describe("Upload code", function () {
     var token = new Token({
       cluestrToken: '123TEST',
       dropboxTokens: config.test_tokens,
-      cursor:"AAHGAZxS20rjQgBQVVulcPzUCYmNf2m1KUpcEmsWOFnhGFpdDhSSRIgG-WAQQeNLYxq51l5AJDDtfD1Fox6vytTP0gn7ou7PL6Z3cMs2IADgjo2ED-YYyVLwP9D5XXaDYQlQb5vEiOtfLPu3sxObDGseyveffqn03r8iotTvoI_-wlxMa0TrngZLKlAgF_xT2lGsdpDBIxiFLXXCG-CadZvA2_2ncaCJQTjJh7ODmQ4o7pU32kOEpHsBgB31PfUP4t0"
+      cursor:config.test_cursor
     });
 
     async.series([
       // Create a fake CluestrToken
-      async.apply(token.save),
-
-      // Retrieve associated contacts
-      async.apply(providerDropbox.helpers.upload(cb));
+      function(cb) {
+        token.save(cb);
+      },
+      // Retrieve files
+      async.apply(providerDropbox.helpers.upload, token)
     ], done);
   });
 });
