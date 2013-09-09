00e5a9c62accb397070fc3ef8a5ae4595a9c99d9
Neamar <neamar@neamar.fr>
2013-09-09 16:16:43 +0200

    All tests running

--- test/update.js
+++ test/update.js
@@ -10,30 +10,30 @@ var providerDropbox = require('../lib/provider-dropbox');
 var Token = providerDropbox.models.Token;
 
 describe("POST /upload", function () {
-  it("should not raise any exception", function (done) {
-    // It is quite hard to really test the upload code,
-    // Therefore we'll only check no errors are raised.
-    // For faster test, we won't upload.
-    config.cluestr_url = 'http://test/';
+  // It is quite hard to really test the upload code,
+  // Therefore we'll only check no errors are raised.
+  // For faster test, we won't upload.
+  config.cluestr_url = 'http://test/';
 
-    var token = new Token({
+  var token;
+  beforeEach(function(done) {
+    token = new Token({
       cluestrToken: '123TEST',
       dropboxTokens: config.test_tokens,
       cursor:config.test_cursor
     });
 
-    async.series([
-      // Create a fake CluestrToken
-      function(cb) {
-        token.save(cb);
-      },
-      // Retrieve files
-      function(cb) {
-        var req = request(app).post('/update')
-          .send({access_token: token.cluestrToken})
-          .expect(204)
-          .end(done);
-      }
-    ], done);
+    token.save(done);
+  });
+
+  it("should not raise any exception", function (done) {
+    var req = request(app).post('/update')
+      .send({access_token: token.cluestrToken})
+      .expect(204)
+      .end(done);
+  });
+
+  it("should upload all changes since", function(done) {
+    providerDropbox.helpers.upload(token, done);
   });
 });
