bb649736f3debce47906a37792a99c1956a91248
buathi_q <buathi_q@epitech.eu>
2014-11-20 16:25:43 +0100

    Change app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var anyfetchProvider = require('anyfetch-provider');
 var serverConfig = require('./lib/');
 
 
-var server = anyfetchProvider.createServer(serverConfig.connectFunctions, serverConfig.updateAccount, serverConfig.workers, serverConfig.config);
+var server = anyfetchProvider.createServer(serverConfig.connectFunctions, __dirname + '/lib/workers.js', __dirname + '/lib/update.js', serverConfig.config);
 server.get('/image', require('./lib/handlers/image.js').get);
 
 // Expose the server
